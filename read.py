9018e813f3afd30b901fff465d136ede229f0d30
Arjun V <arjun.v@invenics.com>
2023-11-21 18:11:56 +0530

    read file added

---
+++ read.py
@@ -0,0 +1,15 @@
+#open an existing file for read operation
+# file = open("name.txt", "r") 
+# print (file.read())
+#To overwrite an existing data
+# file = open("name.txt", "w") 
+# file.write("I am sai eashwar")
+# file.close()
+#To append an existing file and it won't overwrite 
+# file = open("name.txt", "a") 
+# file.write("\nThis will add this line")
+# file.close()
+#To append and open a existing file and it won't overwrite
+file = open("name.txt", "a+") 
+file.write("\nThis will add this line")
+file.close()
